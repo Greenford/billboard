f7deaa425f748a4b87bcf7b628256abc9ad7539e
Greenford <lauren.n.greenford@gmail.com>
2020-02-20 20:37:18 -0600

    bb scraper working

--- src/billboard_scraper.py
+++ src/billboard_scraper.py
@@ -1,3 +1,27 @@
-import billboard
+import json, billboard
+from pymongo import MongoClient
+from datetime import datetime
+
+def run(verbose=1):
+    collection = MongoClient()['billboard']['hot100']
+
+    kwargs = {'name':'hot-100'}
+    date = billboard.ChartData(**kwargs).date
+    while date:
+        kwargs['date'] = date
+        chart = billboard.ChartData(**kwargs)
+        weekdata = json.loads(chart.json())
+        weekdata['_id'] = weekdata['date']
+        weekdata['scraped'] = datetime.utcnow()
+        collection.insert_one(weekdata)
+
+        if verbose:
+            print(date)
+
+        date = chart.previousDate
 
 
+def clean():
+    pass
+
+run()
