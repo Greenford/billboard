f521b0e0dcdcfb07c8f6281e470b8e8ba1fc2eb5
Greenford <lauren.n.greenford@gmail.com>
2020-02-21 16:54:22 -0600

    billboard data cleaning

--- src/billboard_scraper.py
+++ src/billboard_scraper.py
@@ -21,11 +21,28 @@ def run(date=None, verbose=1):
             print(date)
         
         date = chart.previousDate
-        time.sleep(2)
+         time.sleep(2)
 
 def clean():
     """
     Cleans raw data from the billboard scraper.
     """
-    pass
+    db = MongoClient().billboard 
+    hot100filtered = db['hot100filtered']
+    hot100raw = db.hot100
+
+    df = pd.DataFrame(hot100raw.find())\
+        .drop(columns=['_id', '_max_retries', '_timeout', 'name', 'nextDate', 'title'])
+    alltracks=[]
+    for row in df.values:
+        date = row[0]
+        for track in row[1]:
+            track['date'] = date
+            alltracks.append(track)
+    df = pd.DataFrame(alltracks)
+    df = df.groupby(['artist', 'title'])\
+        .agg({'date':min, 'peakPos':max, 'weeks':max}, axis='columns').reset_index()
+    for i in range(df.shape[0]):
+        hot100filtered.insert_one(df.iloc[i].to_dict())
+
 
