aaa61af975a57a6fd624daef35a39875e9f62da9
Ubuntu <ubuntu@ip-172-31-22-119.ec2.internal>
2020-02-25 22:55:18 +0000

    complete pipe

--- src/pipe.py
+++ src/pipe.py
@@ -3,24 +3,22 @@ import numpy as np
 from pymongo import MongoClient
 from functools import reduce
 from operator import add
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 class BillboardData(object):
     def __init__(self):
-        db = MongoClient().billboard
-        df1 = load_spotify_billboard_data(db)
-        df2 = load_spotify_nillboard_data(db)
-        lyrics_df = load_lyrics_data(db)
-        adf = load_spotify_album_data(db)
-        bbdf = load_hot_100_data(db)
-        self.df = (
-            df1.append(df2, ignore_index=True, sort=False)
-            .merge(right=lyrics_df, how="outer", on="track_id")
-            .merge(right=adf, how="left", on="album_id")
+        self.db = MongoClient().billboard
+        df1 = self.load_spotify_billboard_data()
+        df2 = self.load_spotify_nillboard_data()
+        lyrics_df = self.load_lyrics_data()
+        adf = self.load_spotify_album_data()
+        bbdf = self.load_hot_100_data()
+        self.df = df1.append(df2, ignore_index=True, sort=False)\
+            .merge(right=lyrics_df, how="outer", on="track_id")\
+            .merge(right=adf, how="left", on="album_id")\
             .merge(right=bbdf, how="left", on="obj_id")
-        )
 
-    def load_spotify_bb_data(db):
+    def load_spotify_billboard_data(self):
         return pd.DataFrame(
             map(
                 lambda r: [
@@ -49,9 +47,9 @@ class BillboardData(object):
                     r["audio_features"]["valence"],
                     r["audio_features"]["tempo"],
                     r["audio_features"]["time_signature"],
-                    r["_id"],
+                    r["_id"]
                 ],
-                db.spotify.find(),
+                self.db.spotify.find()
             ),
             columns=[
                 "artist",
@@ -79,11 +77,11 @@ class BillboardData(object):
                 "valence",
                 "tempo",
                 "time_signature",
-                "obj_id",
-            ],
+                "obj_id"
+            ]
         )
 
-    def load_spotify_nillboard_data(db):
+    def load_spotify_nillboard_data(self):
         return pd.DataFrame(
             map(
                 lambda r: [
@@ -114,7 +112,7 @@ class BillboardData(object):
                     r["audio_features"]["time_signature"],
                     None,
                 ],
-                db.spotify_nillboard.find(),
+                self.db.spotify_nillboard.find(),
             ),
             columns=[
                 "artist",
@@ -146,7 +144,7 @@ class BillboardData(object):
             ],
         )
 
-    def load_lyrics_data(db):
+    def load_lyrics_data(self):
         return pd.DataFrame(
             map(
                 lambda r: [
@@ -157,7 +155,7 @@ class BillboardData(object):
                     r["dict_sentiment"]["neg"],
                     r["dict_sentiment"]["wordcount"],
                 ],
-                db.lyrics.find(),
+                self.db.lyrics.find(),
             ),
             columns=[
                 "track_id",
@@ -169,7 +167,7 @@ class BillboardData(object):
             ],
         )
 
-    def load_hot_100_data(db):
+    def load_hot_100_data(self):
         return pd.DataFrame(
             map(
                 lambda r: [
@@ -180,29 +178,27 @@ class BillboardData(object):
                     r["peakPos"],
                     r["weeks"],
                 ],
-                db.hot100filtered.find(),
+                self.db.hot100filtered.find(),
             ),
-            columns=["obj_id", "bb_artist", "bb_title", "date", "peakPos", "weeks"],
+            columns=["obj_id", "bb_artist", "bb_title", "date_entered_bb", "peakPos", "weeks"]
         )
 
-    def load_spotify_album_data(db):
+    def load_spotify_album_data(self):
         # None of the genres are filled
-        adf = pd.DataFrame(
+        return pd.DataFrame(
             map(
                 lambda r: [r["_id"], r["label"], r["popularity"]],
-                db.spotify_albums.find(),
+                self.db.spotify_albums.find(),
             ),
             columns=["album_id", "label", "album_popularity",],
         )
 
-        self.df = (
-            df1.append(df2, ignore_index=True, sort=False)
-            .merge(right=lyrics_df, how="outer", on="track_id")
-            .merge(right=adf, how="left", on="album_id")
-            .merge(right=bbdf, how="left", on="obj_id")
-        )
-
     def transform_for_models(self):
+        # Drop if they don't have lyrics 
+        haslyrics = ~self.df.response_title.isna()
+        self.df = self.df[haslyrics].reset_index()
+        
+
         self.df["on_billboard"] = ~self.df.obj_id.isna()
         self.df.release_date = pd.to_datetime(self.df.release_date, format="%Y-%m-%d")
         self.df["norm_sentiment"] = (self.df.poscount - self.df.negcount) / (
@@ -216,8 +212,55 @@ class BillboardData(object):
             lambda r: r.release_date.month
             if r.release_date_precision == "day"
             else np.nan,
-            axis=1,
+            axis=1
         )
+        self.df['track_placement'] = self.df.track_number/self.df.total_tracks + 1 - 1/self.df.disc_number
+        self.df.
+        self.df.explicit = self.df.explicit.astype(np.uint8)
+        self.df.on_billboard = self.df.on_billboard.astype(np.uint8)
+        self.df = pd.get_dummies(self.df, columns=['album_type', 'key', 'time_signature', 'release_month'])
+        # Drop unneeded columns
+        self.df.drop(columns=[
+            'track_id',
+            'obj_id',
+            'poscount',
+            'negcount',
+            'artist',
+            'album_id',
+            'release_date_precision',
+            'release_date',
+            'disc_number',
+            'track_number',
+            'title',
+            'response_artist',
+            'response_title',
+            'bb_artist',
+            'bb_title',
+            'peakPos',
+            'weeks',
+            'date_entered_bb',
+            'rel_sentiment',
+        ], inplace=True)
+
+    def dummyize_record_label(self, min_label_size=12):
+        # Turns each track's list of labels into a space-separated string of labels
+        self.df.label = self.df.label.apply(lambda l: " ".join([''.join(lword.split()) for lword in l.split('/')]).lower())
+        vectorizor = CountVectorizer()
+        counts = vectorizor.fit_transform(self.df.label).toarray()
+        counts = pd.DataFrame(counts, columns = map(lambda s: 'label_'+s, 
+            vectorizor.get_feature_names()), dtype=np.uint8)
+        labels_start_idx = self.df.shape[1]
+        self.df = self.df.merge(counts, left_index=True, right_index=True)
+        
+        lt_label=f'lt_{min_label_size}_label'
+        self.df.insert(labels_start_idx, lt_label, 0)
+        labels_start_idx += 1
+        
+        for column in self.df.columns[labels_start_idx:]:
+            if np.sum(self.df[column]) < 12:
+                self.df[lt_label] = self.df[lt_label]+self.df[column]
+                self.df.drop(columns=[column], inplace=True)
+        self.df.drop(columns=['label'], inplace=True)
 
     def transform_for_EDA(self):
         pass
