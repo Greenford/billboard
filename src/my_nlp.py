30ebeef18257bce3ae3d4080c081eca174d91978
Ubuntu <ubuntu@ip-172-31-22-119.ec2.internal>
2020-02-25 02:23:04 +0000

    pipe nearly complete, waiting for lyrics to finish

--- src/my_nlp.py
+++ src/my_nlp.py
@@ -18,22 +18,24 @@ class Sentimenter:
                 pcount += 1
             elif word in self.neg:
                 ncount += 1
-        return (pcount-ncount)/(pcount+ncount+1), pcount, ncount
+        return {'sentiment':(pcount-ncount)/(pcount+ncount+1), 
+                'pos':pcount, 
+                'neg':ncount, 
+                'wordcount':len(words)}
     
-def process_mongo_docs():
-    collection = MongoClient('localhost', 27017).tracks.lyrics
+def process_mongo_docs(verbose=1):
+    collection = MongoClient().billboard.lyrics
     results = collection.find({'$and':[{'_id':{'$exists':'true'}}, 
                               {'lyrics':{'$exists':'true'}}]})
     s = Sentimenter()
     count = 0
     for track in results:
         if track.get('dict_sentiment')==None:
-            score = s.sentiment(track['lyrics'])
-            collection.update({'_id':track['_id']}, {'$set':{'dict_sentiment':score}})
+            scores = s.sentiment(track['lyrics'])
+            collection.update({'_id':track['_id']}, {'$set':{'dict_sentiment':scores}})
             count += 1
-            print(count)
-        else:
-            count += 1
+            if verbose:
+                print(count)
 
 if __name__ == '__main__':
     process_mongo_docs()
